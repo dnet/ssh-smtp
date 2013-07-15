227b42a2044031dfb3fbe504cffcd9905087bd02
András Veres-Szentkirályi <vsza@vsza.hu>
2013-07-15 19:48:58 +0200

    added SMTP hello and HELO/MAIL FROM handling

--- server.cpp
+++ server.cpp
@@ -1,6 +1,8 @@
 #include "server.h"
 #include <iostream>
 
+#define BUFSIZE 1024
+
 using namespace std;
 
 Server::Server(QObject* parent): QObject(parent)
@@ -21,13 +23,24 @@ void Server::acceptConnection()
 	client = server.nextPendingConnection();
 	connect(client, SIGNAL(readyRead()),
 			this, SLOT(startRead()));
+
+	client->write("220 ssh-smtp.vsza.hu ESMTP SSH-SMTP\r\n");
 }
 
 void Server::startRead()
 {
-	char buffer[1024] = {0};
-	client->read(buffer, client->bytesAvailable());
-	cout << buffer << endl;
-	client->close();
-}
+	char buffer[BUFSIZE];
 
+	if (client->readLine(buffer, BUFSIZE)) {
+		QString line(buffer);
+
+		if (line.startsWith("HELO ")) {
+			client->write("250 Hello ");
+			client->write(buffer + 5);
+		}
+		else if (line.startsWith("MAIL FROM:")) {
+			cout << buffer + 10 << endl; // TODO
+			client->close();
+		}
+	}
+}
