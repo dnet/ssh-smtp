34504a0124d98f9e9d6b89adbfbeb0a8ae9b76eb
András Veres-Szentkirályi <vsza@vsza.hu>
2013-07-16 07:27:48 +0200

    added application metadata for QSettings

--- main.cpp
+++ main.cpp
@@ -4,5 +4,10 @@
 int main(int argc, char **argv) {
 	QCoreApplication app(argc, argv);
 	Server server;
+
+	QCoreApplication::setOrganizationName("dnet");
+	QCoreApplication::setOrganizationDomain("vsza.hu");
+	QCoreApplication::setApplicationName("ssh-smtp");
+
 	return app.exec();
 }
